9a1b72dea42be4ea13ffe42eaa9c6f42c3616677
Victor Rosas <victor.rosas.96@hotmail.com>
2021-04-26 16:45:55 -0500

    finished hero image slider

---
+++ src/data/SliderData.js
@@ -0,0 +1,66 @@
+import ImageOne from '../images/house-1.jpeg';
+import ImageTwo from '../images/house-2.jpeg';
+import ImageThree from '../images/house-3.jpeg';
+import ImageFour from '../images/house-4.jpeg';
+
+export const SliderData = [
+  {
+    title: 'Luxury Villa in Bali, Indonesia',
+    price: '$4,280,00',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageOne,
+    alt: 'House'
+  },
+  {
+    title: 'House in San Jose, Costa Rica',
+    price: '$2,860,00',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageTwo,
+    alt: 'House'
+  },
+  {
+    title: 'House in Calabasas, California',
+    price: '$8,4200,00',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageThree,
+    alt: 'House'
+  },
+  {
+    title: 'House in Houston, Texas',
+    price: '$26,660,00',
+    path: '/homes',
+    label: 'View Home',
+    image: ImageFour,
+    alt: 'House'
+  }
+];
+
+// export const SliderDataTwo = [
+//   {
+//     title: 'House in Las Vegas, Nevada',
+//     price: '$8,890,00',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageFive,
+//     alt: 'House'
+//   },
+//   {
+//     title: 'House in Houston, Texas',
+//     price: '$3,460,00',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageSix,
+//     alt: 'House'
+//   },
+//   {
+//     title: 'House in Alajuela, Costa Rica',
+//     price: '$5,420,00',
+//     path: '/homes',
+//     label: 'View Home',
+//     image: ImageSeven,
+//     alt: 'House'
+//   },
+// ]
